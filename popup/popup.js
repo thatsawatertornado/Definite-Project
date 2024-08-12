25385610b01edbc3fa3681648f1b5afaab2ce284
Windlag <sethneoehgmail.com>
2024-08-12 18:02:02 +0800

    see https://www.youtube.com/watch?v=dFVxUrd1wzA&list=PLVrGZCP4x3PRHKbq-gDrSygGHbU4X9pTR&index=5 time 4min37s

--- popup/popup.js
+++ popup/popup.js
@@ -7,14 +7,20 @@ const StopButton = document.getElementById("stop_button");
 const StartButton = document.getElementById("start_button");
 
 StartButton.onclick = function() {
+    const prefs = {
+        https://www.youtube.com/watch?v=dFVxUrd1wzA&list=PLVrGZCP4x3PRHKbq-gDrSygGHbU4X9pTR&index=5
+    }
+    chrome.runtime.sendMessage({event: 'onStart'});
     console.log("You clicked the start button!");
-    console.log("Rehab is in progress for: ", RehabSelection.value);
     //this doesnt seem to work rn
+    //send the input data to background.js for processing using the "sendMessage" Chrome API
  
 }
 StopButton.onclick = function() {
+    chrome.runtime.sendMessage({event: 'onStop'});
     console.log("You clicked the stop button!");
 }
+
 
 
 
--- scripts/bakground.js
+++ scripts/bakground.js
@@ -1,9 +1,17 @@
-chrome.runtime.onMessage.addListener(data => {
-    switch (dataEvent) {
-        case 'OnStop':
-            break;
-        case 'OnStart':
-            break;
+//example of what data object looks like
+/* let data = {
+    "event": "thumbnail_blocker, Bolivia, etc"
+    "prefs": {
+    "banana": "banana"
+    }
+} */
+//message listener
+chrome.runtime.onMessage.addListener(data => { //data is sent from popup.js events
+    switch (data.event) {
+        case 'onStart':
+            console.log("start in background");
+        case 'onStop':
+            console.log("start in background");
         default:
             break;
     }
