814da374a7f47bb317120949cbed1292e414677a
Windlag <sethneoehgmail.com>
2024-08-14 17:18:45 +0800

    expanded.

--- scripts/bakground.js
+++ scripts/bakground.js
@@ -6,18 +6,24 @@
     }
 } */
 //message listener
-chrome.runtime.onMessage.addListener(data => { //data is sent from popup.js events
-    switch (data.event) {
+chrome.runtime.onMessage.addListener( (data) => { //"data" is sent from popup.js events. This is a fat arrow function
+    let {event, prefs} = data;
+    switch (event) {
         case 'onStop':
-            console.log("stop in background");
+            handleonStop();
             break;
         case 'onStart':
-            console.log("start in background");
-            console.log("prefs received: ", data.prefs);
+            handleonStart(prefs);
             break;
-
-
         default:
             break;
     }
 })
+const handleonStop = () => {
+    console.log("stop in background");
+}
+
+const handleonStart = (prefs) => {
+    console.log("start in background");
+    console.log("prefs received: ", prefs);
+}
