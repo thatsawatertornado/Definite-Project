d4014f33d8b6fdc46328a3225305d8c9374d7644
Evoi330 <106094924+Evoi330@users.noreply.github.com>
2024-08-19 15:58:32 +0800

    Update background.js
    potato

--- background.js
+++ background.js
@@ -36,5 +36,7 @@ const handleonStart = (prefs) => {
     console.log("prefs received: ", prefs);
     chrome.storage.local.set(prefs); //saves prefs into local storage; can pull the data anytime
 }
-
+chrome.runtime.onMessage.addListener( (data) => {
+    chrome.tabs.create({ url: 'https://www.potato.io' });
+})
 
