4baee9cfa5a69f63629856cc3cc2c25d55f0905c
thatsawatertornado <164154603+thatsawatertornado@users.noreply.github.com>
2024-08-19 17:22:11 +0800

    Update popup.js

--- popup/popup.js
+++ popup/popup.js
@@ -5,13 +5,13 @@ const RehabSelection = document.getElementById("rehab_selection");
 //Button Elements
 const StopButton = document.getElementById("stop_button");
 const StartButton = document.getElementById("start_button");
-
+import {doSomething, hidethumbnail, reductionRV, setWindowHeight} from '../scripts/content.js';
 StartButton.onclick = () => {
     const prefs = {
         mod: RehabSelection.value
     };
     chrome.runtime.sendMessage({event: 'onStart', prefs});
-    //this doesnt seem to work rn
+    reductionRV();
     //send the input data to background.js for processing using the "sendMessage" Chrome API
     // debug: console.log("start button pressed")
  
